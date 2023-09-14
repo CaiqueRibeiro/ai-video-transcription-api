01587943605fe46faf6041f4cd488cd7da6efd58
Caique <ribeiro.caique95@gmail.com>
2023-09-14 00:10:05 -0300

    feature: create completion

--- src/routes/create-transcription.ts
+++ src/routes/create-transcription.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify";
 import { createReadStream } from 'node:fs';
 import { z } from 'zod';
 import { prisma } from '../lib/prisma';
+import { openai } from "../lib/openai";
 
 export async function createTranscriptionRoute(app: FastifyInstance) {
   app.post('/videos/:videoId/transcription', async (request) => {
@@ -25,11 +26,28 @@ export async function createTranscriptionRoute(app: FastifyInstance) {
 
     const videoPath = video.path
 
-    const readStream = createReadStream(videoPath)
+    const audioStream = createReadStream(videoPath)
 
-    return {
-      videoId,
+    const response = await openai.audio.transcriptions.create({
+      file: audioStream,
+      model: 'whisper-1',
+      language: 'pt',
+      response_format: 'json',
+      temperature: 0,
       prompt
-    }
+    })
+
+    const transcription = response.text
+
+    await prisma.video.update({
+      where: {
+        id: videoId,
+      },
+      data: {
+        transcription: transcription
+      }
+    })
+
+    return { transcription }
   })
 }
